8ace0251a9bf4274fc9873fd99ccbfdbe7726e86
viniciuskirsten <vinicius.k.mendonca@outlook.com>
2025-03-18 13:22:57 -0300

    Começando o curso de arrays e colecoes

---
+++ bytebank_ATENDIMENTO/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Boas Vindas ao ByteBank, Atendimento.");
+
+void testaArrayInt()
+{
+    //uma das maneiras que eu tenho para escrever um array em C#
+    int[] idades = new int[5];
+
+    //adicionando valores através de indices
+    idades[0] = 30;
+    idades[1] = 40;
+    idades[2] = 17;
+    idades[3] = 21;
+    idades[4] = 18;
+    
+    Console.WriteLine($"Tamanho do array: {idades.Length}");
+
+    for (int i = 0; i < idades.Length; i++)
+    {
+    }
+}
